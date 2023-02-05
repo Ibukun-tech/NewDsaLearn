b5c561b33589b8662c08f6e42bf5c443e30e9c54
Ibukun-tech <oyetunjiibukunoluwa8@gmail.com>
2023-02-05 20:30:48 +0100

    longest common prefix

---
+++ longestCommonPrefix.py
@@ -0,0 +1,12 @@
+# Time complexity is o(n) and space complexity is o(1)
+
+def longestCommonPrefix(s):
+  st=s[0]
+  res=''
+  for i in range(st):
+    for j in s:
+      if st[i]!=j[i]:
+        return res
+      res+=st[i]
+  return res
+    
